0fd3e420966d1003d28c17a3a811bb68d62bbcf8
Sfiriac <bianca-loredana.sfiriac@garmin.com>
2018-02-21 13:56:21 +0200

    First commit

---
+++ Library/app/src/main/java/com/example/sfiriac/library/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.sfiriac.library
+
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleOwner
+
+/**
+ * Created by sfiriac on 2/21/2018.
+ */
+
+class MainActivity: LifecycleOwner {
+
+    override fun getLifecycle(): Lifecycle {
+
+        return null;
+    }
+
+}
